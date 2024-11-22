792e852a2c257aff771dbd6b42683b918ecca7c0
Emanuel Rivas <Emanuelrivasdavid@gmail.com>
2024-11-22 13:53:15 -0500

    Updated Options

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import json
 # appl = stockClass("/fileForApple")
 # amzn = stockClass()
 # goog = stockClass()
+# optional = stockClass("")
+
 
 # data_source = "./stock-data/AAPL_DATA.json"
 # with open(data_source, "r") as file:
@@ -16,10 +18,12 @@ WHOLE GAME
 # def select_option(number):
 
 
+
+
 question_Prompt = '''
 =========================================================
 Enter a number to select one of the following
-    [1] - View LATEST stock data
+    [1] - View LATEST stock data 
     [2] - View LOWEST trading price
     [3] - View HIGHEST trading price
     [4] - View all stock data on a SPECIFIC date 
@@ -32,7 +36,8 @@ print('''Welcome! Enter a number to access data on a specific stock!
         [2] - AAPL (Apple)
         [3] - AMZN (Amazon)
         [4] - NTFX (Netflix)
-        [5] - GOOG (Google)''')
+        [5] - GOOG (Google)
+        [6] - Input your own JSON stock data filepath''')
 
 user_input = int(input())
 
@@ -40,6 +45,7 @@ if user_input == 1:
     print("")
     print("Accessing Microsoft...")
     print(question_Prompt)
+
 elif user_input == 2:
     print("")
     print("Accessing Apple...")
